a2d6ed105839757777d10ace2a1be7779b0ec9d9
Stasysolnze <Stasysolnze@yandex.ru>
2021-12-02 14:42:57 +0300

    Добавьте файлы проекта.

---
+++ HomeWork_5_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            const int m = 5;
+
+            int[,] t = new int[n, m];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    t[i, j] = 1 - (i + j) % 2;
+                    {
+                        Console.Write("{0,4} ", t[i, j]);
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework_5_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 7;
+            int[] array = new int[n];
+            Random random = new Random();
+            int S = 0;
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next(0, 20);
+                S += array[i];
+                Console.Write("{0} ", array[i]);
+            }
+            {
+                Console.WriteLine();
+                Console.WriteLine("Сумма чисел в массиве: ");
+                Console.Write(S);
+                Console.WriteLine();
+                Console.WriteLine("Среднее арифметичекое: ");
+                Console.Write(S / n);
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
+
